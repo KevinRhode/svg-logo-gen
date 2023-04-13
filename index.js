97ffcf77b986d89ab0a5cc3094009f6d16dd6894
Kevin Rhode <kevinrhode@hotmail.com>
2023-04-12 22:33:18 -0400

    add: Starting README, code completed checking for bugs, checking comments

--- index.js
+++ index.js
@@ -24,31 +24,38 @@ const questions = ()=>{
         [
         // WHEN I am prompted for text  
         // THEN I can enter up to three characters
+        // note input type, uses imported prompt
         new Question('text.text','maxlength-input','Enter up to 3 Characters...','',3),
         // WHEN I am prompted for the text color
         // THEN I can enter a color keyword (OR a hexadecimal number)
-        new Question('text.color','input','Enter Color: keyword, or hex; hex must include # at the begining to be used'),
+        new Question('text.color','input','Enter Text Color: keyword, or hex; hex must include # at the begining to be used'),
         // WHEN I am prompted for a shape
         // THEN I am presented with a list of shapes to choose from: circle, triangle, and square
         new Question('shape','list','Select Shape...',['Triangle','Circle','Square']),
         // WHEN I am prompted for the shape's color
         // THEN I can enter a color keyword (OR a hexadecimal number)
-        new Question('color','input','Enter Color: keyword, or hex; hex must include # at the begining to be used')
+        new Question('color','input','Enter Shape Color: keyword, or hex; hex must include # at the begining to be used')
         ]
         )
     .then((response)=>{
         console.log(response);        
         //error check
+        //AC Check THEN I can enter up to three characters
         if (response.text.text.length > 3) {
+            //tell user info on why
             throw new Error('Your Text was longer then 3.');
         }
+        //AC Check THEN I can enter up to three characters
         if (!colorHelper(response.text.color)) {
+            //tell user info on why
             throw new Error('Your Text color is not valid');
         }
+        //AC Check THEN I can enter a color keyword (OR a hexadecimal number)
         if (!colorHelper(response.color)) {
+            //tell user info on why
             throw new Error('Your shape color is not valid');
         }
-
+        
 
         return response;
     })
@@ -64,8 +71,10 @@ const questions = ()=>{
 // WHEN I open the `logo.svg` file in a browser
 // THEN I am shown a 300x200 pixel image that matches the criteria I entered  
 
+//function to return ture if passing name or hex test.
 const colorHelper = (color)=>{
 
+    //hex test
     if (color.includes('#')) {
         const hexPattern = '^#(?:[0-9a-fA-F]{3}){1,2}$'
         const regex = new RegExp(hexPattern);
@@ -74,7 +83,9 @@ const colorHelper = (color)=>{
         }
         
 
-    } else {
+    } 
+    //name test
+    else {
         for (const {colorName} of testColor) {
             if (colorName.toLocaleLowerCase() === color.toLocaleLowerCase()) {
                 return true;
@@ -85,6 +96,7 @@ const colorHelper = (color)=>{
     return false;
     
 }
+//create file from response
 const makeLogo = (response)=>{
 
     const{text,shape,color}=response;
@@ -109,13 +121,14 @@ const makeLogo = (response)=>{
         default:
             break;
     }
+    //render string from obj and text
     data = renderText(obj,text);
     // THEN an SVG file is created named `logo.svg` 
     writeFile('./examples/logo.svg', data);
 
 }
-
-const renderText = function(obj,text) {
+//takes and square, triangle, or circle and creates the svg, get defaults of size from default options on obj
+const renderText = function(obj,{text}) {
     const {version,width,height,xmlns} = obj;
     return`<svg version="${version}"
         width="${width}" height="${height}"
@@ -125,12 +138,12 @@ const renderText = function(obj,text) {
         </svg>`;
     
 }
-
+//enter into questions
 const init =()=>{
     questions();
 }
 
-
+//start the program
 init();
 
 
